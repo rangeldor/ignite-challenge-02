af164e4800a63d651150926e464bce57dfcbca16
Daniel Oliveira Rangel <rangeldor@gmail.com>
2021-08-30 17:22:10 -0300

    Feito a SideBar e Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
+import { useState } from 'react';
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  genres: [{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }]
+}
+
+export function SideBar({ genres }: GenreResponseProps) {
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
   // Complete aqui
+  return (<nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>)
 }
